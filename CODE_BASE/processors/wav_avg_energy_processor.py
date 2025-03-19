993c57e35fb8cbd870bd68a0fe0b318cd105bad7
= <yonatankarby@gmail.com>
2025-03-19 09:30:24 +0200

    Added the calculation of the average energy of audio

--- CODE_BASE/processors/wav_avg_energy_processor.py
+++ CODE_BASE/processors/wav_avg_energy_processor.py
@@ -1,4 +1,5 @@
 from scipy.io import wavfile
+import numpy as np
 import io
 
 from .multiprocessing_processor import MultiprocessingProcessor
@@ -10,4 +11,13 @@ class WavAvgEnergyProcessor(MultiprocessingProcessor):
         wav_data: bytes = args['wav_data']
         io_stream = io.BytesIO(wav_data)
         sample_rate, data = wavfile.read(io_stream)
-        return 0.0
+        data_casted: np.ndarray = data
+
+        if len(data_casted.shape) > 1:
+            data_casted = data_casted.mean(axis=1)
+
+        energy = np.square(data_casted)
+
+        average_energy = np.mean(energy)
+
+        return average_energy
